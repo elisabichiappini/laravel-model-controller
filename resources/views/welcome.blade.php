1ee9f616d3345eb2876317b4e9fb88dcc764476d
elisabichiappini <elisabichiappini@gmail.com>
2024-01-29 21:10:20 +0100

    feat: creating cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Model Controller</title>
     <!--inclusione assets con direttiva vite-->
     @vite('resources/js/app.js')
     <!--/inclusione assets con direttiva vite-->
 </head>
-<body>
-    <h1>Template</h1>
+<body class="bg-dark">
+    <header class="container py-5">
+        <h1 class="text-center text-white">Movies</h1>
+    </header>
+    <main class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+            <div class="col-3 px-0 card border-green mb-3">
+                <div class="card-header">{{ $movie['title']}}</div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </main>
+    
 </body>
 </html>
