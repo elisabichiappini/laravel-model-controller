921933a6dd44daf2bad8548d5bd819af94b6a700
elisabichiappini <elisabichiappini@gmail.com>
2024-01-29 20:53:48 +0100

    feat: connection to database

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+// individuato model con il namespace
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
+//estendiamo a PageController la classe Controller
 class PageController extends Controller
 {
     public function index() {
-        return view('welcome');
+        //metodo statico per prendere tutto il contenuto della tabella movies
+        $movies = Movie::all();
+        return view('welcome', ['movies' => $movies]);
     }
 }
